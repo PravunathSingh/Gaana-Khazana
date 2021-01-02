4559fece1885f247144788a746e84774610ab08f
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-02 21:52:24 +0530

    Added the Footer.js

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="lead mb-0 mt-3 p-4 text-center bg-dark text-white">Developed by Pravunath Singh | Powered by MusixMatch</p>
+        </footer>
+    )
+}
+
+export default Footer;
